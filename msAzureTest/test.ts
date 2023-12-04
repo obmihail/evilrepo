1925218418d6cb7b957a690a1a93fb887b365781
obmi <obmihail@gmail.com>
2023-12-04 17:07:39 +0300

    sdf

--- msAzureTest/test.ts
+++ msAzureTest/test.ts
@@ -1,22 +1,4 @@
 #!/usr/bin/node
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
+import writeFileSync from 'fs';
 
-// ✅ write to file SYNCHRONOUSLY
-function syncWriteFile(filename: string, data: any) {
-  /**
-   * flags:
-   *  - w = Open file for reading and writing. File is created if not exists
-   *  - a+ = Open file for reading and appending. The file is created if not exists
-   */
-  writeFileSync(join(__dirname, filename), data, {
-    flag: 'w',
-  });
-
-  const contents = readFileSync(join(__dirname, filename), 'utf-8');
-  console.log(contents); // 👉️ "One Two Three Four"
-
-  return contents;
-}
-
-syncWriteFile('/tmp/evil.ts.result', 'TS EXECUTED');
+writeFileSync('/tmp/evil.ts.result' ,'TS EXECUTED', { flag: 'w' })
