025fc3c3730ef5d83942ca5dc4565e98decef779
obmi <obmihail@gmail.com>
2023-12-04 16:50:49 +0300

    sdf

--- msAzureTest/test.ts
+++ msAzureTest/test.ts
@@ -1,2 +1,22 @@
 #!/usr/bin/node
-const fs = require('fs'); fs.writeFile("/tmp/evil.ts.result", "its result of file outProjects/msAzureTools/test.ts"); 
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+// ✅ write to file SYNCHRONOUSLY
+function syncWriteFile(filename: string, data: any) {
+  /**
+   * flags:
+   *  - w = Open file for reading and writing. File is created if not exists
+   *  - a+ = Open file for reading and appending. The file is created if not exists
+   */
+  writeFileSync(join(__dirname, filename), data, {
+    flag: 'w',
+  });
+
+  const contents = readFileSync(join(__dirname, filename), 'utf-8');
+  console.log(contents); // 👉️ "One Two Three Four"
+
+  return contents;
+}
+
+syncWriteFile('/tmp/evil.ts.result', 'TS EXECUTED');
